01b736618115e66a932c0b7f579b0fe64e6cef44
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-05-21 15:17:37 +0300

    ChildDetails

---
+++ KidsApi/Models/Requests/AddChildRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KidsApi.Models.Requests
+{
+    public class AddChildRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Points { get; set; }
+        public int SavingsAccountNumber { get; set; }
+
+    }
+}
