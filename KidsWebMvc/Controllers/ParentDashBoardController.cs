0f3a6ead93935e592dd5ca02688b1b7cc01b581a
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-05-30 10:37:33 +0300

    added

---
+++ KidsWebMvc/Controllers/ParentDashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KidsWebMvc.Controllers
+{
+    public class ParentDashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
