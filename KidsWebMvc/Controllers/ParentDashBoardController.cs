646c297c793270401d317667809909516b14779a
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-06-02 10:26:25 +0300

    adding mvc functions

--- KidsWebMvc/Controllers/ParentDashBoardController.cs
+++ KidsWebMvc/Controllers/ParentDashBoardController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KidsWebMvc.API;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KidsWebMvc.Controllers
 {
     public class ParentDashBoardController : Controller
     {
-        public IActionResult Index()
+        private readonly KidsApiClient _client;
+        public ParentDashBoardController(KidsApiClient client)
+        {
+            _client = client;
+        }
+        public async Task<IActionResult> Index(int parentId)
+        {
+            var child = await _client.GetChildren(parentId);
+            return View(child);
+        }
+
+        public async Task<IActionResult> Details()
+        {
+
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> AddChild()
         {
             return View();
         }
+
+        public async Task<IActionResult> AddTask()
+        {
+            return View();
+        }
+        public async Task<IActionResult> TaskJistory()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddReward()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Requests()
+        {
+            return View();
+        }
+
+
+
     }
 }
